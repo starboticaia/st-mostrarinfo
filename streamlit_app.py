ab4f4055593b5add8e299056d952b83adbd3c5c0
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-07 19:20:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,9 @@ import streamlit as st
 
 st.write("Texto simple")
 
-st.write("""
-#Encabezado 1
-##Encabezado 2
-###Encabezado 3
-####Encabezado 4
-#####Encabezado 5
-######Encabezado 6
-""")
+st.write("#Encabezado 1")
+st.write("##Encabezado 2")
+st.write("###Encabezado 3")
+st.write("####Encabezado 4")
+st.write("#####Encabezado 5")
+st.write("######Encabezado 6")
