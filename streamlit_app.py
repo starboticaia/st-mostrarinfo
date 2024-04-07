0bd938e1dd233662b2f332b0f1727bf72aa7b9d6
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-07 19:32:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.title("Título")
 st.header("Cabecera")
 st.subheader("Subcabecera")
-st.write("Texto simple")
+st.write("Texto simple con emoji :sunglasses:")
 
 st.markdown("*negrita* **cursiva** ***negrita cursiva***")
 
-st.write("Códigos de emojis para streamlit https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/")
+st.write("Códigos de emojis para streamlit: https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/")
