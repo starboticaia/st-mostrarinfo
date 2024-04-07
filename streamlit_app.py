548c5deaac6f998fec6bb5d0604217af111e12b0
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-07 19:46:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,12 @@ st.write("Texto simple con emoji :sunglasses:")
 st.write("Códigos de emojis para streamlit: https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/")
 st.caption("Texto en fuente pequeña con caption.")
 
+st.divider()
+
 st.markdown("Texto usando Markdown: *negrita* **cursiva** ***negrita cursiva***")
 st.markdown(":red[Texto] de :rainbow[colores] con :violet[Markdown]")
+
+st.divider()
 
 multilinea = """
 Cuando deseamos escribir  
