27d24898be048bad987ead3ac694de698d4339b2
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-07 19:45:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ st.header("Cabecera")
 st.subheader("Subcabecera")
 st.write("Texto simple con emoji :sunglasses:")
 st.write("Códigos de emojis para streamlit: https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/")
+st.caption("Texto en fuente pequeña con caption.")
 
 st.markdown("Texto usando Markdown: *negrita* **cursiva** ***negrita cursiva***")
 st.markdown(":red[Texto] de :rainbow[colores] con :violet[Markdown]")
